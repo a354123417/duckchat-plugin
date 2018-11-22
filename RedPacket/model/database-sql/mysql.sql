e80713a1515fb03d4bdfe432945c6798b426c4af
SAM2O2O <an.guoyue254@gmail.com>
2018-11-22 17:34:39 +0800

    update redPacket sql

--- RedPacket/model/database-sql/mysql.sql
+++ RedPacket/model/database-sql/mysql.sql
@@ -1,10 +1,10 @@
 
-CREATE TABLE IF NOT EXISTS RedPacketAccount (
+CREATE TABLE IF NOT EXISTS DuckChatUserAccount (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   userId VARCHAR(100) unique not null,
   amount decimal(4,2))DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;
 
-CREATE TABLE IF NOT EXISTS RedPacketRecords (
+CREATE TABLE IF NOT EXISTS DuckChatRedPacketRecords (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   packetId VARCHAR(50) unique not null,
   userId VARCHAR(100) not null,
@@ -13,9 +13,10 @@ CREATE TABLE IF NOT EXISTS RedPacketRecords (
   description VARCHAR(100),
   isGroup boolean,
   roomId VARCHAR(100) not null,
-  sendTime BIGINT)DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;
+  sendTime BIGINT,
+  finishTime BIGINT)DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;
 
-CREATE TABLE IF NOT EXISTS RedPacketGrabbers (
+CREATE TABLE IF NOT EXISTS DuckChatRedPacketGrabbers (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   packetId VARCHAR(50) not null,
   userId VARCHAR(100) not null,
